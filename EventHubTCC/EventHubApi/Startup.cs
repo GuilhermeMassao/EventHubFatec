0b23572849891a3e79ca9511be71480027acbebf
Rodrigo de Oliveira Soares <rodrigosoares@lnxcit016553.cit>
2019-09-11 15:27:51 -0300

    Swagger configuration, TwiterController structure created

--- EventHubTCC/EventHubApi/Startup.cs
+++ EventHubTCC/EventHubApi/Startup.cs
@@ -10,6 +10,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using SocialConnection.Connections;
+using SocialConnection.Connections.Interfaces;
+using SocialConnection.Data;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace EventHubApi
 {
@@ -26,6 +31,16 @@ namespace EventHubApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            
+            //Swagger Configuration
+            services.AddSwaggerGen(
+                c => c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "EventHub API"
+                })
+            );
+            //IoC configuration
+            services.AddTransient<ITwitterConnection<ClientTwitterAccessTokenData>, TwitterConnection>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +57,14 @@ namespace EventHubApi
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            
+            //Swagger Configuration
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "EventHub API");
+                });
         }
     }
 }
