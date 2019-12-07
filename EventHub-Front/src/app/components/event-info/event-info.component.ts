5bff684223d57308517a4e3f49dbf2d8c2945ec3
matbio <matheus.biosalles@gmail.com>
2019-12-07 19:19:43 -0300

    Exibição dos eventos com botões edição e exclusão

--- EventHub-Front/src/app/components/event-info/event-info.component.ts
+++ EventHub-Front/src/app/components/event-info/event-info.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { EventService } from 'src/app/services/event.service';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-event-info',
@@ -7,9 +11,90 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventInfoComponent implements OnInit {
 
-  constructor() { }
+  eventId: any;
+
+  eventInfo = {
+    eventName: 'Nome do Evento',
+    eventDescription: 'Descrição longa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd',
+    startDate: '12/12/12',
+    endDate: '13/12/12',
+    ticketsLimit: '10'
+  };
+
+  adressInfo = {
+    adressId: '',
+    publicPlaceId: '',
+    placeName: 'rua lala',
+    city: 'Toronto',
+    uf: 'Ontario',
+    cep: '13215-472',
+    neighborhood: 'Victorias Park',
+    adressComplement: 'Rua sem saida',
+    adressNumber: '234'
+  };
+
+  constructor(private eventService: EventService, private router: Router, private toastr: ToastrService,  private activatedRoute: ActivatedRoute) { }
+
+  fillEvent() {
+    this.activatedRoute.queryParams.subscribe(params => {
+      if(params.id != null) {
+        this.eventId = params.id;
+        this.eventService.getEventById(params.id).subscribe(
+          (res: any) => {
+            this.fillInfoEvent(res);          },
+          err => {
+            if(err.status == 400) {
+              console.log("Error 400");
+              this.toastr.error('Tente novamente mais tarde.','Erro ao preencher informações do evento!').onHidden.subscribe(() => {
+                // this.router.navigateByUrl("/eventhub/home");
+              });
+            }
+            else {
+              console.log("err");
+              this.toastr.error('Tente novamente mais tarde.','Erro ao preencher informações do evento!').onHidden.subscribe(() => {
+                // this.router.navigateByUrl("/eventhub/home");
+              });
+            }
+          }
+        );
+      }
+      else {
+        this.router.navigateByUrl("/eventhub/home");
+      }
+    });
+  }
+  fillInfoEvent(data: any) {
+    this.eventInfo = {
+      eventName: data.eventName,
+      eventDescription: data.eventDescription,
+      startDate: data.startDate,
+      endDate: data.endDate,
+      ticketsLimit: data.ticketsLimit
+    };
+
+    this.adressInfo = {
+      adressId: data.adressId,
+      publicPlaceId: data.publicPlaceId,
+      placeName: data.placeName,
+      city: data.city,
+      uf: data.uf,
+      cep: data.cep,
+      neighborhood: data.neighborhood,
+      adressComplement: data.adressComplement,
+      adressNumber: data.adressNumber
+    };
+  }
+
+  toEditEvent() {
+    this.router.navigateByUrl("/eventhub/editar-evento?id=" + this.eventId);
+  }
+
+  toDeleteEvent() {
+
+  }
 
   ngOnInit() {
+    this.fillEvent();
   }
 
 }
